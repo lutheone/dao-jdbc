d8f72f1d556b1ed352f3eac46c3346d8272c6899
Luiz Henrique Oliveira <contato3luizhenriqueoliveira@gmail.com>
2025-09-11 18:44:16 -0300

    feat: Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Department dep = new Department(1, "Books");
-        System.out.println(dep);
 
+        Seller seller = new Seller(1, "Bob", "bob@gmail.com",
+                new java.util.Date(), 3000.0, dep);
 
+        System.out.println(seller);
     }
 }
