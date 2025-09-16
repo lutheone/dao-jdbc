57cba0a3c808ace90c6540bb845727c128a99fae
Luiz Henrique Oliveira <contato3luizhenriqueoliveira@gmail.com>
2025-09-15 23:03:23 -0300

    feat: department insert implementation

---
+++ src/application/Program2Test.java
@@ -0,0 +1,29 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2Test {
+
+    public static void main(String[] args) {
+
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+        System.out.println("=== TEST1 INSERT DEPARTMENT ===");
+        Department dep = new Department(null, "Music");
+        departmentDao.insert(dep);
+        System.out.println("Inserted! New id = " + dep.getId());
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
